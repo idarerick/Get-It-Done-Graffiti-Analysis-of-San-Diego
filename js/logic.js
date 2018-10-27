2147866595655940f86eeec7819d03751ad57e0a
darrick <darrick.suen@gmail.com>
2018-10-27 10:00:11 -0700

    updated maps

--- js/logic.js
+++ js/logic.js
@@ -1,6 +1,6 @@
 // Creating map object
 var map = L.map("map", {
-  center: [32.7157, -117.1611],
+  center: [32.82, -117.1611],
   zoom: 11
 });
 
@@ -14,35 +14,54 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 
 var link = "../council_districts_datasd.geojson";
 
-// Function that will determine the color of a neighborhood based on the district it belongs to
+// Function that will determine the color of a district based on the district it belongs to
 function chooseColor(district) {
   switch (district) {
-  case "1":
-    return "yellow";
-  case "2":
-    return "red";
-  case "3":
-    return "orange";
-  case "4":
+  case 1:
+    return "teal";
+  case 2:
+    return "blue";
+  case 3:
     return "green";
-  case "5":
+  case 4:
+    return "orange";
+  case 5:
     return "purple";
+  case 6:
+    return "brown";
+  case 7:
+    return "yellow";
+  case 8:
+    return "red";
+  case 9:
+    return "magenta";
   default:
     return "black";
   }
 }
 
+
+
+// var WriterzBlok = {
+//   location: [32.710720, -117.086959],
+//   name: "Writerz Blok"
+// };
+
+// var marker = L.marker(WriterzBlok).addTo(myMap);
+
+
+
 // Grabbing our GeoJSON data..
-d3.json(link, function(data) {
+d3.json(link, function(geojsondata) {
   // Creating a geoJSON layer with the retrieved data
-  L.geoJson(data, {
-    // Style each feature (in this case a neighborhood)
+  L.geoJson(geojsondata, {
+    // Style each feature (in this case a district)
     style: function(feature) {
       return {
         color: "white",
-        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+        // Call the chooseColor function to decide which color to color our district (color based on district)
         fillColor: chooseColor(feature.properties.district),
-        fillOpacity: 0.5,
+        fillOpacity: 0.3,
         weight: 1.5
       };
     },
@@ -54,23 +73,23 @@ d3.json(link, function(data) {
         mouseover: function(event) {
           layer = event.target;
           layer.setStyle({
-            fillOpacity: 0.9
+            fillOpacity: 0.7
           });
         },
         // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
         mouseout: function(event) {
           layer = event.target;
           layer.setStyle({
-            fillOpacity: 0.5
+            fillOpacity: 0.3
           });
         },
-        // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+        // When a feature (district) is clicked, it is enlarged to fit the screen
         click: function(event) {
           map.fitBounds(event.target.getBounds());
         }
       });
       // Giving each feature a pop-up with information pertinent to it
-      layer.bindPopup("<h1>" + feature.properties.neighborhood + "</h1> <hr> <h2>" + feature.properties.borough + "</h2>");
+      layer.bindPopup("<h1> District " + feature.properties.district + "</h1> <hr> <h2>" + feature.properties.district + "</h2>");
 
     }
   }).addTo(map);
