dd7ef3941f2bf95af46e29c73471782d3e1f34d6
darrick <darrick.suen@gmail.com>
2018-10-27 10:04:15 -0700

    quick update

--- js/logic.js
+++ js/logic.js
@@ -89,7 +89,7 @@ d3.json(link, function(geojsondata) {
         }
       });
       // Giving each feature a pop-up with information pertinent to it
-      layer.bindPopup("<h1> District " + feature.properties.district + "</h1> <hr> <h2>" + feature.properties.district + "</h2>");
+      layer.bindPopup("<h1> District " + feature.properties.district + "</h1> <hr> <h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.phone + "</h3> <hr> <h4>" + feature.properties.website + "</h4>");
 
     }
   }).addTo(map);
